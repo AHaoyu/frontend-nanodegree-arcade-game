cc3a89c1c6d197c6997f3632290d7e8608fa2427
aihaoyu <aihaoyu2014@163.com>
2018-01-31 22:46:33 +0800

    add star and heart

--- js/app.js
+++ js/app.js
@@ -24,10 +24,16 @@ Enemy.prototype.update = function(dt) {
       && player.y + 73 <= this.y + 135
       && player.x + 76 >= this.x + 11) {
       console.log('collison detected!');
-      player.blink(true);
+      inputAvailable = 0;
+      player.blink();
       player.reset();
+      hearts.pop();
+      setTimeout(function () {
+          inputAvailable = 1;
+          blinkOrnot = 1;
+      }, 1200);
+
     }
-    //blinkornot = 1;
     if (this.x >= 505) {
       this.x = 0;
     }
@@ -45,7 +51,7 @@ Enemy.prototype.render = function() {
 var Player = function(x, y, speed) {
     this.x = x;
     this.y = y;
-    this.speed = speed
+    this.speed = speed;
     this.sprite = 'images/char-boy.png';
 };
 
@@ -63,6 +69,11 @@ Player.prototype.update = function() {
         this.x = 202.5;
         this.y = 383;
         console.log('Success!');
+        var i = stars.length;
+        stars[i+1] = new Star(380.5+20*i, 525);
+        if (stars.length >= 3) {
+          console.log('You win the Game!!!');
+        }
     }
 };
 
@@ -71,68 +82,92 @@ Player.prototype.reset = function() {
     this.y = 383;
 };
 
-Player.prototype.blink = function(TF) {
+Player.prototype.blink = function() {
     var player_image = Resources.get(this.sprite);
-    if (TF === true) {
-        var status = 1;
-        var blinkTime = 0;
-        var blinking = function () {
-            if (status === 1) {
-                blinkornot = 1;
-                console.log("visible");
-                status = 0;
-                blinkTime += 1;
-            } else {
-                blinkornot = 0;
-                console.log("hidden");
-                status = 1;
-                blinkTime += 1;
-            }
-        };
-        interval = setInterval(blinking,100);
-        setTimeout(function () {
-            clearInterval(interval);
-            blinkornot = 1;
-        }, 1200);
-    } else {
-        blinkornot = 1;
-        //player_image.style.visibility = 'visible';
-        console.log("visible");
-    }
+    var status = 1;
+    var blinkTime = 0;
+    var blinking = function () {
+        if (status === 1) {
+            blinkOrnot = 1;
+            //console.log("visible");
+            status = 0;
+            blinkTime += 1;
+        } else {
+            blinkOrnot = 0;
+            //console.log("hidden");
+            status = 1;
+            blinkTime += 1;
+        }
+    };
+    interval = setInterval(blinking,100);
+    setTimeout(function () {
+        clearInterval(interval);
+        blinkOrnot = 1;
+    }, 1200);
 };
 
 Player.prototype.render = function() {
-    if (blinkornot === 1) {
+    if (blinkOrnot === 1) {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 };
 
 Player.prototype.handleInput = function(inputKeyb){
-switch (inputKeyb) {
-  case 'left':
-    player.x -= player.speed;
-    break;
-  case 'up':
-    player.y -= player.speed;
-    break;
-  case 'right':
-    player.x += player.speed;
-    break;
-  case 'down':
-    player.y += player.speed;
-    break;
-  default:
-    console.log("your inut is invaild!!");
- } 
-}
+  if (inputAvailable === 1) {
+    switch (inputKeyb) {
+      case 'left':
+        player.x -= player.speed;
+        break;
+      case 'up':
+        player.y -= player.speed;
+        break;
+      case 'right':
+        player.x += player.speed;
+        break;
+      case 'down':
+        player.y += player.speed;
+        break;
+      default:
+        console.log("your inut is invaild!!");
+     }
+  }
+};
+
+var Heart = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = 'images/Heart.png';
+};
+
+Heart.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 40, 60);
+};
+
+var Star = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = 'images/Star.png';
+};
+
+Star.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 40, 60);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-var blinkornot = 1;
+var inputAvailable = 1;
+var blinkOrnot = 1;
 var interval = null;
 var player = new Player(202.5, 383, 50);
+var hearts = [];
+for (var i=0;i<3;i++) {
+  hearts[i] = new Heart(5.5+40*i, 530);
+}
+var stars = [];
+
+
 var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
 allEnemies.push(enemy);
 
--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,7 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
@@ -117,7 +117,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
@@ -152,8 +152,14 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
+        hearts.forEach(function(heart) {
+            heart.render();
+        });
+        stars.forEach(function(star) {
+            star.render();
+        });
         player.render();
+        //hearts.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -173,7 +179,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/Heart.png',
+        'images/Star.png'
     ]);
     Resources.onReady(init);
 
